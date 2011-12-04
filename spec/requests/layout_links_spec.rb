2972242a6138ca8b1f36183e04d5e55b1e152a3e
Ali Hannani <alirezah@vt.edu>
2011-12-04 01:14:16 -0800

    Finished layouts and routes including the signup page

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,23 +2,28 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
   
-  it "should have a Home page at '/'" do
+  it "should have a home page at '/'" do
     get '/'
     response.should have_selector('title', :content => "Home")
   end
   
-  it "should have a Contact page at '/Contact'" do
-    get '/Contact'
+  it "should have a contact page at '/Contact'" do
+    get '/contact'
     response.should have_selector('title', :content => "Contact")
   end
   
-  it "should have a About page at '/About'" do
-    get '/About'
+  it "should have a about page at '/About'" do
+    get '/about'
     response.should have_selector('title', :content => "About")
   end
   
-  it "should have a Help page at '/Help'" do
-    get '/Help'
+  it "should have a help page at '/Help'" do
+    get '/help'
     response.should have_selector('title', :content => "Help")
+  end
+  
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
   end
 end
